ad12d62ff3b67753c2e12ce32962fa21d41904cd
fabricuser1 <fabricuser1@devilocloudprepoutlook.onmicrosoft.com>
2025-04-04 15:35:21 +0000

    Committing 12 items from workspace db418ed0-3c3a-4cd3-ad7f-08d470d103b5

---
+++ 01.Raw to Landing.Notebook/notebook-content.py
@@ -0,0 +1,75 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# PARAMETERS CELL ********************
+
+today_file = 'file' #'LMS_09-02-2023.csv'
+processed_Date = '9999-99-99' #'2025-03-24'
+source_account = 'datalake22143' # fill in your primary account name 
+source_container = 'fabricproject' # fill in your container name 
+src_relative_path = 'raw' # fill in your relative folder path 
+destination_account = 'datalake22143'
+destination_container = ''
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+adls_path = 'abfss://%s@%s.dfs.core.windows.net/%s' % (source_container, source_account, src_relative_path) 
+print('Source storage account path is ', adls_path)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+latest_path = f"{adls_path}/{today_file}"
+print(latest_path)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import lit
+latest_path = f"{adls_path}/{today_file}"
+df = spark.read.csv(path= latest_path,header=True,inferSchema=True)
+
+if df.count() > 1:
+    print("The file has data.")
+    
+    df_new = df.withColumn("Processing_Date",lit(processed_Date))
+    df_new.write.format('csv').option('header','true').partitionBy('Processing_Date').mode('append').save(f'abfss://{destination_container}@{destination_account}.dfs.core.windows.net/landing/')
+    print("Data written to landing zone successfully !")
+
+else:
+    print('This file contains only header row and no data.')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
